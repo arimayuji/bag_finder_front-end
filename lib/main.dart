0d4f3b6fec4d15b98138e5bc90fb1dcb51aa4e09
Yuji Arima <yujiarima@hotmail.com>
2024-10-09 08:27:52 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
+import 'package:bag_finder_frontend/app/app_module.dart';
+import 'package:bag_finder_frontend/app/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  return runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ),
+  );
 }
