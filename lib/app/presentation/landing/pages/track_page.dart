1c1a65682ad6abb652681a49d9dbcbfcac58c060
Yuji Arima <yujiarima@hotmail.com>
2024-10-09 08:29:10 -0300

    track page

--- lib/app/presentation/landing/pages/track_page.dart
+++ lib/app/presentation/landing/pages/track_page.dart
@@ -1,10 +1,71 @@
+import 'package:bag_finder_frontend/app/presentation/landing/controller/landing_page_step_progess.dart';
+import 'package:bag_finder_frontend/app/presentation/landing/widgets/progress_indicator.dart';
+import 'package:bag_finder_frontend/app/shared/themes/app_colors.dart';
+import 'package:bag_finder_frontend/app/shared/themes/app_dimensions.dart';
+import 'package:bag_finder_frontend/app/shared/themes/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class TrackPage extends StatelessWidget {
   const TrackPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final controller = Modular.get<LandingPageStepProgess>();
+
+    return Scaffold(
+      backgroundColor: AppColors.secondary,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: AppDimensions.paddingMedium,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(
+              child: Image.asset(
+                'images/luggage-two-persons.png',
+                filterQuality: FilterQuality.high,
+              ),
+            ),
+            ProgressIndicatorWidget(
+              controller: controller,
+            ),
+            Text(
+              AppLocalizations.of(context)!.landingPageSecondTitle,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: AppDimensions.paddingSmall,
+              ),
+              child: Text(
+                AppLocalizations.of(context)!.landingPageSecondText,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  controller.nextPage();
+                },
+                child: Text(
+                  AppLocalizations.of(context)!.landingPageButtonTextNext,
+                  style: AppTextStyles.button,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
