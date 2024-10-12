fce2e415dec369c1bec4daa322302a723b362ae9
Yuji Arima <yujiarima@hotmail.com>
2024-10-12 15:36:44 -0300

    feat: :sparkles: home page

--- lib/app/presentation/home/pages/home_page.dart
+++ lib/app/presentation/home/pages/home_page.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:bag_finder_frontend/app/presentation/home/widget/appbar/home_app_bar_widget.dart';
+import 'package:bag_finder_frontend/app/shared/themes/app_dimensions.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30.0),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: const HomeAppBarWidget(),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(
+            right: AppDimensions.paddingMedium,
+            left: AppDimensions.paddingMedium,
+            bottom: AppDimensions.paddingMedium,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [],
+          ),
+        ),
+      ],
+    );
   }
 }
