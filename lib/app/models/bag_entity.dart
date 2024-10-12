70e5875799b94ae281b7063c4b7601172af0cc13
Yuji Arima <yujiarima@hotmail.com>
2024-10-12 15:26:25 -0300

    chore: :fire: remove code

--- lib/app/models/bag_entity.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bag_finder_frontend/app/models/user_entity.dart';
-
-class BagEntity {
-  final String id;
-  final bool isDelivered;
-  final UserEntity user;
-
-  BagEntity({required this.id, required this.user, required this.isDelivered});
-}
