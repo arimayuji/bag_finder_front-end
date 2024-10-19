c6e6b90425ebf6912973b8d38bcb5c4ce335efa1
Yuji Arima <yujiarima@hotmail.com>
2024-10-19 10:08:50 -0300

    chore: :construction: user repository

--- lib/app/data/repositories/user_repository_mock.dart
+++ lib/app/data/repositories/user_repository_mock.dart
@@ -1,78 +1,310 @@
 import 'package:bag_finder_frontend/domain/entity/trip_entity.dart';
 import 'package:bag_finder_frontend/domain/entity/user_entity.dart';
+import 'package:bag_finder_frontend/domain/failures/failure.dart';
 import 'package:bag_finder_frontend/domain/repositories/user_repository.dart';
+import 'package:collection/collection.dart';
+import 'package:dartz/dartz.dart';
 
 class UserRepositoryMock implements IUserRepository {
-  final UserEntity _userAdmin = UserEntity(
-    phone: '(11) 99999-9999',
-    isAdmin: true,
-    id: '1',
-    email: 'bagfinder@gmail.com',
-    password: 'password!',
-    name: 'Admin',
-    photoUrl: '',
-  );
+  final List<UserEntity> _users = [
+    UserEntity(
+      phone: '(11) 99999-9999',
+      id: '1',
+      email: 'bagfinder@gmail.com',
+      password: 'password!',
+      fullName: 'Admin',
+      photoUrl: '',
+    ),
+    UserEntity(
+      phone: '(11) 99999-9999',
+      id: '1',
+      email: 'bagfinder@gmail.com',
+      password: 'password!',
+      fullName: 'Fake Admin',
+      photoUrl: '',
+    ),
+  ];
 
   @override
-  Future<UserEntity?> authenticateUser({
+  Future<Either<AuthError, UserEntity>> addUser(
+      {required UserEntity user}) async {
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
+
+    UserEntity? isRegistered = _users.firstWhereOrNull(
+      (
+        u,
+      ) {
+        return u.email == user.email;
+      },
+    );
+
+    if (isRegistered == null) {
+      _users.add(
+        user,
+      );
+
+      return Right(user);
+    }
+
+    return Left(
+      UserNotFound(
+        errorMessage: 'User not found',
+        stackTrace: null,
+      ),
+    );
+  }
+
+  @override
+  Future<Either<Failure, UserEntity?>> authenticateUser({
     required String email,
     required String password,
   }) async {
-    await Future.delayed(const Duration(seconds: 2));
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
 
-    if (email != _userAdmin.email || password != _userAdmin.password) {
-      return null;
+    UserEntity user = _users.firstWhere(
+      (
+        user,
+      ) =>
+          user.email == email,
+    );
+
+    if (user is StateError) {
+      return Left(
+        UserNotFound(
+          errorMessage: 'User not found',
+          stackTrace: null,
+        ),
+      );
     }
 
-    return _userAdmin;
+    if (user.password != password) {
+      return Left(
+        UserNotFound(
+          errorMessage: 'User not found',
+          stackTrace: null,
+        ),
+      );
+    }
+
+    return Right(
+      user,
+    );
   }
 
   @override
-  Future<void> addUser({required UserEntity user}) {
-    // TODO: implement addUser
-    throw UnimplementedError();
+  Future<Either<Failure, void>> deleteUser({
+    required String id,
+  }) async {
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
+
+    UserEntity user = _users.firstWhere(
+      (
+        user,
+      ) =>
+          user.id == id,
+    );
+
+    if (user is StateError) {
+      return Left(
+        UserNotFound(
+          errorMessage: 'User not found',
+          stackTrace: null,
+        ),
+      );
+    }
+
+    _users.remove(
+      user,
+    );
+
+    return const Right(
+      null,
+    );
   }
 
   @override
-  Future<void> deleteUser({required String id}) {
-    // TODO: implement deleteUser
-    throw UnimplementedError();
+  Future<Either<Failure, List<UserEntity>>> getAllUsers() async {
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
+
+    return Right(
+      _users,
+    );
   }
 
   @override
-  Future<List<UserEntity>> getAllUsers() {
-    // TODO: implement getAllUsers
-    throw UnimplementedError();
+  Future<Either<Failure, List<TripEntity>>> getUserBagsHistory({
+    required String userId,
+  }) async {
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
+
+    UserEntity user = _users.firstWhere(
+      (
+        user,
+      ) =>
+          user.id == userId,
+    );
+
+    if (user is StateError) {
+      return Left(
+        UserNotFound(
+          errorMessage: 'User not found',
+          stackTrace: null,
+        ),
+      );
+    }
+
+    return Right(
+      user.bags,
+    );
   }
 
   @override
-  Future<List<TripEntity>> getUserBagsHistory({required String userId}) {
-    // TODO: implement getUserBagsHistory
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntity>> getUserByEmail({
+    required String email,
+  }) async {
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
+
+    UserEntity? user = _users.firstWhere(
+      (
+        user,
+      ) =>
+          user.email == email,
+    );
+
+    return Right(
+      user,
+    );
   }
 
   @override
-  Future<UserEntity?> getUserByEmail({required String email}) {
-    // TODO: implement getUserByEmail
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntity?>> getUserById({required String id}) async {
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
+
+    UserEntity? user = _users.firstWhere(
+      (
+        user,
+      ) =>
+          user.id == id,
+    );
+
+    if (user is StateError) {
+      return Left(
+        UserNotFound(
+          errorMessage: 'User not found',
+          stackTrace: null,
+        ),
+      );
+    }
+
+    return Right(
+      user,
+    );
   }
 
   @override
-  Future<UserEntity?> getUserById({required String id}) {
-    // TODO: implement getUserById
-    throw UnimplementedError();
+  Future<Either<Failure, void>> updateUser({required UserEntity user}) async {
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
+
+    UserEntity user = _users.firstWhere(
+      (
+        user,
+      ) =>
+          user.id == user.id,
+    );
+
+    if (user is StateError) {
+      return Left(
+        UserNotFound(
+          errorMessage: 'User not found',
+          stackTrace: null,
+        ),
+      );
+    }
+
+    _users.remove(
+      user,
+    );
+
+    _users.add(
+      user,
+    );
+
+    return const Right(
+      null,
+    );
   }
 
   @override
-  Future<void> updateUser({required UserEntity user}) {
-    // TODO: implement updateUser
-    throw UnimplementedError();
-  }
+  Future<Either<Failure, void>> updateUserBags(
+      {required String userId, required List<TripEntity> bags}) async {
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
 
-  @override
-  Future<void> updateUserBags(
-      {required String userId, required List<TripEntity> bags}) {
-    // TODO: implement updateUserBags
-    throw UnimplementedError();
+    UserEntity user = _users.firstWhere(
+      (
+        user,
+      ) =>
+          user.id == userId,
+    );
+
+    if (user is StateError) {
+      return Left(
+        UserNotFound(
+          errorMessage: 'User not found',
+          stackTrace: null,
+        ),
+      );
+    }
+
+    user = user.copyWith(
+      bags: bags,
+    );
+
+    _users.remove(
+      user,
+    );
+
+    _users.add(
+      user,
+    );
+
+    return const Right(
+      null,
+    );
   }
 }
